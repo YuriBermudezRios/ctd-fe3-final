e688ff605906f7d6d642158ee9bb6ef60a0943cb
YuriBermudezRios <yuri2894@gmail.com>
2022-12-10 02:27:52 -0500

    Creación del Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import Home from "./Routes/Home";
 import Detail from "./Routes/Detail";
 import Contact from "./Routes/Contact";
 import Favs from "./Routes/Favs";
+import { useEffect } from "react";
 
 
 function App() {
+  const location = useLocation()
+  const navegation = useNavigate()
+
+  useEffect(() => {
+    if(location.pathname === "/"){
+      navegation("/home")
+    }
+  })
+
   return (
       <div className="App">
           <Navbar/>
